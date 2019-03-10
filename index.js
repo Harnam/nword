2ad6972f9467f97cbf30f6107f0e49b8602ebbf8
Harnam Singh <harnamsinghsandhu04@gmail.com>
2019-03-10 08:06:08 +0000

    fixed bug of express server

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 const user = require('./user.route');
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(require('cors')());
-app.use(express.static('main/dist/main'));
+app.use(express.static(path.join(__dirname, 'main/dist/main')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/user', user);
@@ -14,8 +15,8 @@ mongoose.connect('mongodb://0.0.0.0:27017/blog', { useNewUrlParser: true });
 let db = mongoose.connection;
 db.on('error', err => console.log('MongoDB connection error:', err));
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'main/dist/main/index.html'));
 });
  
 app.listen(8080, (err) => {
